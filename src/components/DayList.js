a08add26ea9e2666650ca4211af02b3352a83c08
Emily Urdaneta <boss@Emilys-MacBook-Pro.local>
2020-08-31 15:07:24 -0600

    create DayList.js

---
+++ src/components/DayList.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import "components/DayListItem.scss";
+import classnames from 'classnames/bind';
+
+export default function DayList() {
+  return (
+    <ul></ul>
+  );
+}
